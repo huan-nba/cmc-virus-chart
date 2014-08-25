d0b553c96f2856b9d2dd50323614c35b78b3f1e2
huannb <huan.nba@gmail.com>
2014-08-26 01:37:49 +0700

    add autoprefixer, experimenting login with flex

--- Brocfile.js
+++ Brocfile.js
@@ -16,5 +16,9 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+var tree = app.toTree();
 
-module.exports = app.toTree();
+var autoprefixer = require('broccoli-autoprefixer');
+tree = autoprefixer(tree, null);
+
+module.exports = tree;
