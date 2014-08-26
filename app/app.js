b015de901ef70baea65652eb8d813890b9e36092
huannb <huan.nba@gmail.com>
2014-08-26 11:42:01 +0700

    Tinh chỉnh màu cho login UI, thêm login route

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 
@@ -6,9 +7,31 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
   modulePrefix: 'cmc-virus-chart', // TODO: loaded via config
-  Resolver: Resolver
+  Resolver: Resolver,
+  ready: function() {
+    console.log("Ember.TEMPLATES: ", Ember.TEMPLATES);
+  }
 });
 
 loadInitializers(App, 'cmc-virus-chart');
-
+//
+//App.ApplicationAdapter = DS.FixtureAdapter;
+//
+//
+//App.User.FIXTURES = [
+//  {
+//    id: 1,
+//    name: 'Joe User',
+//    username: 'joe',
+//    email: 'joe@email.com',
+//    passwordDigest: '$2a$10$wJTPdvpGgzDvkXChrcPyqOQrFFawzGu89B1rZze/lVIcJKWiNeAqS'
+//  },
+//  {
+//    id: 2,
+//    name: 'John User',
+//    username: 'john',
+//    email: 'john smt',
+//    passwordDigest: '"$2a$10$wJTPdvpGgzDvkXChrcPyqOQrFFawzGu89B1rZze/lVIcJKWiNeAqS'
+//  }
+//];
 export default App;
