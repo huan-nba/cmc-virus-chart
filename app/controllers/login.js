7a6e1c5cd2eec382f67623af91340dcbbcb64e77
huannb <huan.nba@gmail.com>
2014-08-27 18:40:16 +0700

    Update cli 0.0.41

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,9 +1,30 @@
 import Ember from 'ember';
-
 export default Ember.Controller.extend({
+  init: function(){
+    console.log(Chart);
+    console.log('hello');
+  },
   actions: {
     login:function (){
-
+      var user = this.get('user');
+      var pass = this.get('pass');
+      this.set('presentLogin', false);
+      if (user === 'huan' && pass === '123'){
+        alert('correct');
+      }
+      else{
+        this.set('isLoginFailed', true);
+      }
     }
-  }
+  },
+
+  token: localStorage.token,
+  tokenChanged: function(){
+    localStorage.token = this.get('token');
+  }.observes('token'),
+
+  presentLogin: true,
+  user: '',
+  pass: '',
+  isLoginFailed: false,
 });
